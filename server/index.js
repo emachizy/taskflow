2108b21a81b2b6d1ba24677a74a6c6489fc59377
emachizy <emachi2011@gmail.com>
2024-12-10 17:27:48 -0800

    Initial project setup for TaskFlow

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
